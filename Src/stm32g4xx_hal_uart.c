6cdb4c3f4d485eb2bd0717fa240f3ff9687f96dc
Rania JMAI <rania.jmai@st.com>
2024-12-09 17:00:17 +0100

    [HAL][UART] Correct references to HAL_UARTEx_WakeupCallback and to HAL_UART_WAKEUP_CB_ID define, according to serie capabilities

--- Src/stm32g4xx_hal_uart.c
+++ Src/stm32g4xx_hal_uart.c
@@ -1078,6 +1078,9 @@ HAL_StatusTypeDef HAL_UART_UnRegisterRxEventCallback(UART_HandleTypeDef *huart)
         reception services:
         (+) HAL_UARTEx_RxEventCallback()
 
+    (#) Wakeup from Stop mode Callback:
+        (+) HAL_UARTEx_WakeupCallback()
+
     (#) In Non-Blocking mode transfers, possible errors are split into 2 categories.
         Errors are handled as follows :
        (+) Error is considered as Recoverable and non blocking : Transfer could go till end, but error severity is
